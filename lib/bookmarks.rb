7ac3a0ea8eb0d3c6c6e441a9a32a4acf9ebaab5d
dtrts <djt210@gmail.com>
2019-08-13 11:08:46 +0100

    GREEN Bookmark class now adds a bookmark to the db

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -10,4 +10,14 @@ class Bookmarks
     result = conn.exec('select url from bookmarks')
     result.map { |row| row['url'] }
   end
+
+  def self.add(url)
+    conn = if ENV['ENVIRONMENT'] == 'test'
+             PG.connect(dbname: 'bookmark_manager_test')
+           else
+             PG.connect(dbname: 'bookmark_manager')
+     end
+
+    conn.exec("insert into bookmarks (url) values (\'#{url}\');")
+  end
 end
