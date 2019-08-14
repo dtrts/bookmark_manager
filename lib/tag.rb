09dcd9fc50682319d6432f2be128af74d18fff63
dtrts <djt210@gmail.com>
2019-08-14 23:02:01 +0100

    GREEN passing all tag spec tests

--- lib/tag.rb
+++ lib/tag.rb
@@ -1,2 +1,39 @@
 class Tag
+  attr_reader :id, :content
+  def initialize(id:, content:)
+    @id = id
+    @content = content
+  end
+
+  def self.all
+    DatabaseConnection.query('select id, content from tags').map { |tag| Tag.new(id: tag['id'], content: tag['content']) }
+  end
+
+  def self.create(content:)
+    result = DatabaseConnection.query("insert into tags (content) values (\'#{content}\') returning id, content").first
+    Tag.new(id: result['id'], content: result['content'])
+  end
+
+  def self.delete(id:)
+    DatabaseConnection.query("delete from tags where id = #{id}")
+  end
+
+  def self.bookmarks(id:)
+    DatabaseConnection.query("
+      select
+        c.id
+        ,c.title
+        ,c.url
+      from
+        tags a
+          join
+            bookmark_tags b
+            on a.id = b.tag_id
+          join
+            bookmarks c
+            on b.bookmark_id = c.id
+      where
+        a.id = #{id}
+      ").map { |bookmark| Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url']) }
+  end
 end
--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -5,15 +5,15 @@ describe Tag do
     tags = Tag.all
     expect(tags.first).to be_a(Tag)
     expect(tags.length).to eq(5)
-    expect(tags.first.id).to eq(1)
-    expect(tags.first.text).to eq('Great!')
+    expect(tags.first.id).to eq('1')
+    expect(tags.first.content).to eq('Great!')
   end
 
-  it 'adds a new Tag' do
-    tag = Tag.new(text: 'Test tag')
+  it 'creates a Tag' do
+    tag = Tag.create(content: 'Test tag')
     retrieved_tag = persisted_data(table: 'tags', id: 6).first
     expect(tag.id).to eq(retrieved_tag['id'])
-    expect(tag.text).to eq(retrieved_tag['text'])
+    expect(tag.content).to eq(retrieved_tag['content'])
   end
 
   it 'deletes a Tag' do
